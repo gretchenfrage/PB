83ea651be8c4acf668d43432d19534ad05f7e747
Phoenix <kahlo.phoenix@gmail.com>
2015-12-05 01:01:40 -0600

    akljndsad

--- src/pbinary/Tester.java
+++ src/pbinary/Tester.java
@@ -1,16 +1,29 @@
 package pbinary;
 
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
 
 public class Tester {
 
-	public static void main(String[] args) {
-		VarInteger i = new VarInteger("cookies", 7);
-		System.out.println(i.getValue());
-		List<Byte> b = i.getBinaryContents();
-		VarInteger i2 = (VarInteger) Variable.constructFromBinary(Variable.VAR_INT, "coooookies", i.getBinaryContents());
-		System.out.println(i2.getValue());
-		System.out.println(i2.getBinary());
+	public static void main(String[] args) throws IOException {
+		File file = new File("test.txt");
+		file.createNewFile();
+		
+		PBinaryFile pb = new PBinaryFile(file);
+		
+		pb.addVariable(new VarBoolean("yes or no", true));
+		pb.addVariable(new VarByte("bittle", (byte) 65));
+		pb.addVariable(new VarChar("cee", 'c'));
+		pb.addVariable(new VarDouble("doble", 231.5234));
+		pb.addVariable(new VarInteger("answer", 42));
+		pb.addVariable(new VarLong("long", 123213123123123L));
+		
+		pb.writeToFile();
+		
+		pb = null;
+		
+		PBinaryFile pb2 = new PBinaryFile(file);
+		System.out.println(pb2.getVariables());
 	}
 	
 }
